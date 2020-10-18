fef7bc85636128e149274af1d2d3bdef593d54f1
EspenP <espenp92@gmail.com>
2020-10-18 10:29:33 -0400

    Added turtle. Travels across screen in a straight line.

--- game.py
+++ game.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
+#This is the basic game file. When done, this will be packed into a function
+#that returns the end of game statistics, which will then be plotted and displayed to the user.
+
 import pygame, sys
 import random
 import numpy as np
+from PIL import Image
 
 """ DEFINE TILES """
 W = 0 #WATER
@@ -29,39 +33,51 @@ TileTexture = {W : WATER,
               X : IMPASSE}
 
 """DEFINE MAP"""
-map1 = np.array([[G, G, G, G, G, G, G, G, R, G, G, G, G, G, G, G, W],
-                [G, G, G, G, G, G, G, G, R, G, G, G, G, G, G, G, W],
-                [G, G, G, G, G, G, G, G, R, G, G, G, G, G, G, G, W],
-                [G, G, G, G, G, G, G, G, R, G, G, G, G, G, G, G, W],
-                [G, G, G, G, G, G, G, G, R, G, G, G, G, G, G, G, W],
-                [G, G, G, G, G, G, G, G, R, G, G, G, G, G, G, G, W],
-                [G, G, G, G, G, G, G, G, R, G, G, G, G, G, G, G, W],
-                [G, G, G, G, G, G, G, G, R, G, G, G, G, G, G, G, W],
-                [G, G, G, G, G, G, G, G, R, G, G, G, G, G, G, G, W],
-                [G, G, G, G, G, G, G, G, R, G, G, G, G, G, G, G, W],
-                [G, G, G, G, G, G, G, G, R, G, G, G, G, G, G, G, W],
-                [G, G, G, G, G, G, G, G, R, G, G, G, G, G, G, G, W]])
+map1 = np.array([[G, G, G, G, G, G, G, G, R, R, G, G, G, G, G, G, W],
+                 [G, G, G, G, G, G, G, G, R, R, G, G, G, G, G, G, W],
+                 [G, G, G, G, G, G, G, G, R, R, G, G, G, G, G, G, W],
+                 [G, G, G, M, M, G, G, G, R, R, G, G, G, G, G, G, W],
+                 [G, G, G, M, M, G, G, G, R, R, G, G, G, G, G, G, W],
+                 [G, G, G, M, M, G, G, G, R, R, G, G, G, G, G, G, W],
+                 [G, G, G, G, G, G, G, G, R, R, G, G, G, G, G, G, W],
+                 [G, G, G, G, G, G, G, G, R, R, G, G, G, G, G, G, W],
+                 [G, G, G, G, G, G, G, G, R, R, G, G, G, G, G, G, W],
+                 [G, G, G, G, G, G, G, G, R, R, G, G, G, G, G, G, W],
+                 [G, G, G, G, G, G, G, G, R, R, G, G, G, G, G, G, W],
+                 [G, G, G, G, G, G, G, G, R, R, G, G, G, G, G, G, W]])
 
 TILESIZE = 40
-MAPWIDTH = len(map1[1])
+MAPWIDTH = len(map1[0])
 MAPHEIGHT = len(map1)
 
 """ GAME VARIABLES """
-# None yet
+img_path = "assets/"
+starting_pos = (50, (MAPHEIGHT*TILESIZE) // 2)
 
 """ INIT GAME """
 pygame.init()
-DISPLAY = pygame.display.set_mode((MAPWIDTH*TILESIZE,MAPHEIGHT*TILESIZE))
-CLOCK = pygame.time.Clock()
+screen = pygame.display.set_mode((MAPWIDTH*TILESIZE,MAPHEIGHT*TILESIZE))
+clock = pygame.time.Clock()
+
+""" GAME OBJECTS """
+# TURTLES
+turtle_img = Image.open(img_path + "turtle.png")
+turtle_img = turtle_img.resize((TILESIZE // 2, TILESIZE // 2))
+turtle_surface = pygame.image.fromstring(turtle_img.tobytes(), turtle_img.size, turtle_img.mode)
+turtle_surface = pygame.transform.rotozoom(turtle_surface, -90, 1)
+turtle_rect = turtle_surface.get_rect(center = starting_pos)
 
 """ FUNCTION DEFS """
 def display_map():
-  """ DRAW MAP TO DISPLAY """
+  """ DRAW MAP TO SCREEN """
   for row in range(MAPHEIGHT):
     for col in range(MAPWIDTH):
-      pygame.draw.rect(DISPLAY,TileTexture[map1[row][col]],(col*TILESIZE,row*TILESIZE,TILESIZE,TILESIZE))
+      pygame.draw.rect(screen,TileTexture[map1[row][col]],(col*TILESIZE,row*TILESIZE,TILESIZE,TILESIZE))
 
+def turtle_safe(trect):
+  return trect.centerx >= MAPWIDTH
 
+""" MAIN LOOP """
 while True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
@@ -69,6 +85,12 @@ while True:
       sys.exit()
 
   display_map()
+
+  # Turtle movement
+  turtle_rect.centerx += 1
+  screen.blit(turtle_surface, turtle_rect)
+
   pygame.display.update()
+  clock.tick(120)
 
 
